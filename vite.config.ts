d3262dbff4403da0ff4456dd41df56494972e9d7
phonzammi <phonzammizaki@gmail.com>
2024-03-05 02:11:54 +0700

    use hono

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,27 @@
 import react from '@vitejs/plugin-react'
 import vike from 'vike/plugin'
+import devServer from "@hono/vite-dev-server"
 import { UserConfig } from 'vite'
 
 const config: UserConfig = {
-  plugins: [react(), vike()]
+  plugins: [
+    react(),
+    vike(),
+    devServer({
+      entry: "./server/index.ts",
+      exclude: [
+        /.*\.ts$/,
+        /.*\.tsx?($|\?)/,
+        /.*\.(s?css|less)($|\?)/,
+        /.*\.(svg|png)($|\?)/,
+        /^\/@.+$/,
+        /^\/favicon\.ico$/,
+        /^\/(public|assets|static)\/.+/,
+        /^\/node_modules\/.*/,
+      ],
+      injectClientScript: false,
+    }),
+  ]
 }
 
 export default config
